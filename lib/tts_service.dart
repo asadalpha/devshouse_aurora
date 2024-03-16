f5d3a8e8a5d251df605c100f3296e9f1c4d4df40
Prabhdeep <deepprabh832@gmail.com>
2024-03-16 14:44:29 +0530

    p

--- lib/tts_service.dart
+++ lib/tts_service.dart
@@ -6,7 +6,7 @@ class TTSService {
   Future<void> initTts() async {
     await _tts.setLanguage('en-US'); // Set the language for text-to-speech
     await _tts.setPitch(1.0); // Set the pitch of the speech output
-    await _tts.setSpeechRate(0.5); // Set the speech rate (0.0 to 2.0)
+    await _tts.setSpeechRate(0.65); // Set the speech rate (0.0 to 2.0)
   }
 
   Future<void> speak(String text) async {
